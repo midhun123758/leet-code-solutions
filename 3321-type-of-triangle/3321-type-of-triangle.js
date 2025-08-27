ea782179cc8c5ed4838094161d74386bd39dbf4b
MIDHUN CHANDRAN T <midhunchandran230@gmail.com>
2025-08-27 21:48:37 +0530

    Time: 1 ms (45.87%), Space: 55.8 MB (11.47%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+nums=nums.sort((a,b)=>a-b)
+if(nums[0] + nums[1]<=nums[2]){
+    return "none"
+}if(nums[0]==nums[2]){
+    return "equilateral"
+}if(nums[0]==nums[1]||nums[1]==nums[2]){
+   return "isosceles"
+}
+else{
+    return "scalene"
+}
+};
