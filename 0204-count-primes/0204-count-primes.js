fae215e1a9e1421842d6d6dd2bfb2dc1d82358e4
MIDHUN CHANDRAN T <midhunchandran230@gmail.com>
2025-11-02 10:46:20 +0530

    Time: 163 ms (70.09%), Space: 96 MB (65.8%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+  if (n <= 2) return 0;
+
+  let isPrime = new Array(n).fill(true);
+  isPrime[0] = isPrime[1] = false;
+
+  for (let i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j < n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (isPrime[i]) count++;
+  }
+
+  return count;
+};
