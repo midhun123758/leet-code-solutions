8d527dfade666d3cfd6fc3814ff0e64acce7a7a5
MIDHUN CHANDRAN T <midhunchandran230@gmail.com>
2026-02-10 23:01:08 +0530

    Time: 1 ms (77.1%), Space: 12.5 MB (79.58%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        data=int(num1)
+        data2=int(num2)
+        data3=data+data2
+
+        
+        return str(data3)
