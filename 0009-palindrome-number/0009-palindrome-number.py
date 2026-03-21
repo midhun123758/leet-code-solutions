b5e1b91cdaca8c698928f1d7936be9665adac909
MIDHUN CHANDRAN T <midhunchandran230@gmail.com>
2026-03-21 12:54:08 +0530

    Time: 10 ms (59.75%), Space: 12.4 MB (44.15%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x <0 :
+            return False
+        num=int(str(x)[::-1])
+        return x==num
